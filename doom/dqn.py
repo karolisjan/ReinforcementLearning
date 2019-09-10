50f89d1ec06baba54350e093463b4fd0ff6bd432
karolisjan <karolis.jankauskas@gmail.com>
2019-09-10 22:50:30 +0100

    Building dqn.py

--- doom/dqn.py
+++ doom/dqn.py
@@ -1,18 +1,27 @@
+from typing import Sequence, Tuple
+
 import tensorflow as tf
+from fire import Fire
 
 
-class DQN:
+class DQN(tf.keras.Model):
     def __init__(
         self,
-        state_size: int,
+        state_size: Tuple[int],
         action_size: int,
         learning_rate: float,
-        name: str='DQN'
-    
+        name: str='DQN',
+        **kwargs
     ):
-        pass
+        super(DQN, self).__init__(name=name, **kwargs)
+
+        self.inputs = tf.placeholder(
+            dtype=tf.float32,
+            shape=[None, *state_size],
+            name='inputs'
+        )
 
 
 if __name__ == "__main__":
-    pass
+    Fire(DQN)
 
