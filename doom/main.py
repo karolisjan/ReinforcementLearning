6f8f2f856379d91d6184977a419e0717307e06ea
karolisjan <karolis.jankauskas@gmail.com>
2019-09-17 23:59:01 +0100

    Adding main.py

--- doom/main.py
+++ doom/main.py
@@ -1,8 +1,67 @@
+from typing import Tuple
+from collections import deque
+
 import vizdoom as vzd
+
+import numpy as np
+from skimage import transform
+from skimage.color import rgb2gray
+
+from dddqn import DDDQN
+from memory import Memory
+
+from fire import Fire
+
+
+def preprocess(
+    frame: np.ndarray,
+    resize: Tuple=(100, 120),
+    to_gray: bool=False
+) -> np.ndarray:
+    out = np.copy(frame)
+
+    if to_gray:
+        out = rgb2gray(out)
+
+    return transform.resize(out / 255.0, resize)
+
+
+def make_state(stack: deque, frame: np.ndarray) -> np.ndarray:
+    stack.append(frame)
+    return np.stack(stack, axis=2)
+
+
+def pretrain(
+    stack: deque,
+    memory: object,
+    actions: np.ndarray,
+    n_pretraining_eps: int
+):
+    for ep in range(n_pretraining_eps):
+       pass
+
+
+def main(
+    config: str,
+    stack_size: int=4,
+    to_gray: bool=False
+) -> None:
+    game = vzd.DoomGame()
+    game.load_config(config)
+    game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
+    game.set_labels_buffer_enabled(True)
+    game.set_depth_buffer_enabled(True)
+    game.set_render_hud(True)
+
+    game.init()
+    game.new_episode()
+    state = game.get_state()
+    frame = state.screen_buffer
+    preprocessed = preprocess(frame, to_gray=to_gray)
+    stack = deque([preprocessed for _ in range(stack_size)], maxlen=stack_size)
+
+    game.close()
 
 
 if __name__ == '__main__':
-    game = vzd.DoomGame()
-    game.add_game_args('+freelook 1')
-    game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
-
+    Fire(main)
